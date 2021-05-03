51336e68ac0647687cbdc7f957748c178a4ad6af
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-05-03 12:40:02 +0200

    finito metodo 1 e 5

--- AllenamentoEfficace/Gestione.cs
+++ AllenamentoEfficace/Gestione.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.IO;
 
 namespace AllenamentoEfficace
 {
     public class Gestione
     {
+        public static readonly string DIRECTORY = Directory.GetCurrentDirectory();
+        public const string FILE = @"\datiBattiti.txt";
         public static string btmMaxMin(int battiti, int eta)
         {
             int freqMax = 220 - eta;
@@ -11,9 +14,29 @@ namespace AllenamentoEfficace
             double freqMaxAll = (freqMax * 90) / 100;
             return ($"{freqMaxAll} {freqMinAll}");
         }
-        public static string LetturafileMediaGiornaliera()
+        public static double LetturafileMediaGiornaliera()
         {
-
+            double media;
+            using (StreamWriter w = new StreamWriter(DIRECTORY+FILE))
+            {
+                Random r = new Random();
+                for(int i = 0; i < 1440; i++)
+                {
+                    w.WriteLine(r.Next(60, 80));
+                }
+                w.Flush();
+            }
+            using(StreamReader sr=new StreamReader(DIRECTORY+FILE))
+            {
+                string line;
+                double somma=0;
+                while ((line =sr.ReadLine()) != null)
+                {
+                    somma += double.Parse(line);
+                }
+                media = somma / 1440;
+            }
+            return Math.Round(media,2);
         }
     }
 }
