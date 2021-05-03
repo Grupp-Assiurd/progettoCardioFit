b654a3623ccdd00e302db8d8e5ae1c68ba7b7b0c
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-05-03 09:08:37 +0200

    metodo 1 e 5 5 da fine

--- AllenamentoEfficace/Gestione.cs
+++ AllenamentoEfficace/Gestione.cs
@@ -4,16 +4,16 @@ namespace AllenamentoEfficace
 {
     public class Gestione
     {
-        public static string btmMaxMin(int battiti,int eta)
+        public static string btmMaxMin(int battiti, int eta)
         {
             int freqMax = 220 - eta;
             double freqMinAll = (freqMax * 70) / 100;
-            double freqMaxAll= (freqMax * 90) / 100;
+            double freqMaxAll = (freqMax * 90) / 100;
             return ($"{freqMaxAll} {freqMinAll}");
         }
         public static string LetturafileMediaGiornaliera()
         {
-            
+
         }
     }
 }
