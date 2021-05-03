17144f0d37baa326744271f6b414564ad63cb063
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2021-05-03 09:06:10 +0200

    Add project files.

---
+++ AllenamentoEfficace/Gestione.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AllenamentoEfficace
+{
+    public class Gestione
+    {
+        public static string btmMaxMin(int battiti,int eta)
+        {
+            int freqMax = 220 - eta;
+            double freqMinAll = (freqMax * 70) / 100;
+            double freqMaxAll= (freqMax * 90) / 100;
+            return ($"{freqMaxAll} {freqMinAll}");
+        }
+        public static string LetturafileMediaGiornaliera()
+        {
+            
+        }
+    }
+}
